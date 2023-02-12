3fdf6905d34bd59edd3972a79ad5318f7a4be0b4
kosei-ru <kosei.ru@gmail.com>
2023-02-12 21:28:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let codeMorse;
+    let codeBinary;
+
+    for (let i = 0; i < expr.length; i+=10) {
+      codeBinary = expr.slice(i, i + 10);
+      if (codeBinary == '**********') {
+        result += ' ';
+        continue;
+      }
+
+      codeMorse = '';
+      for (let j = 0; j < codeBinary.length; j+=2) {
+        if (codeBinary.slice(j, j+2) == '10') codeMorse += '.';
+        if (codeBinary.slice(j, j+2) == '11') codeMorse += '-';
+      }
+        
+      result += MORSE_TABLE[codeMorse];
+    }
+
+    return result;
 }
 
 module.exports = {
